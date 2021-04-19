7344fb370cf8a4aaabf9c4b6a10bbad592e88780
Starlight Romero <starlightromero@protonmail.com>
2021-04-19 15:32:31 -0700

    [update] sleeper to be configurable

--- countdown_test.go
+++ countdown_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestCountdown(t *testing.T) {
@@ -44,6 +45,18 @@ Go!`
 	})
 }
 
+func TestConfigureableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
+}
+
 type CountdownOperationsSpy struct {
 	Calls []string
 }
@@ -59,3 +72,11 @@ func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
 
 const write = "write"
 const sleep = "sleep"
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
